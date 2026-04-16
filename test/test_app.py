e99d4b33e97be597d2d946b6e9fca505eae98fca
naveen23012005 <naveenchalla341@gmail.com>
2026-04-16 11:52:55 +0530

    3nd

--- test/test_app.py
+++ test/test_app.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+# Add parent directory to path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
 from app import transform_text
 import pickle
 
