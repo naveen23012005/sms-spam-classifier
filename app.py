5847d84aaac8b653d94f2a6618a7a9e617339cce
naveen23012005 <naveenchalla341@gmail.com>
2026-04-16 11:58:26 +0530

    4th

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
 import nltk
 import os
+import nltk
+
+# Ensure required data is available
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
+
+try:
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('stopwords')
 
 nltk_data_path = "/opt/render/nltk_data"
 
